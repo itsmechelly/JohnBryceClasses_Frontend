4348e1c83b6d29fcbf31922e709937bd2e53404d
Chelly izraelov <hlly.lov@gmail.com>
2021-04-01 17:08:36 +0300

    ...

---
+++ 04_React_Ac46/04-this/main.js
@@ -0,0 +1,78 @@
+
+// Get the button object: 
+const myButton1 = document.getElementById("myButton1");
+
+// Add click event to the button:
+myButton1.addEventListener("click", function () {
+    this.style.backgroundColor = "Green";
+});
+
+// -------------------------------------------------------
+
+// this & that: 
+const myButton2 = document.getElementById("myButton2");
+myButton2.addEventListener("click", function () {
+    const that = this; // me, _this, thiz, self, that
+    setTimeout(function () {
+        that.style.backgroundColor = "Green";
+    }, 3000);
+});
+
+// bind:
+const myButton3 = document.getElementById("myButton3");
+myButton3.addEventListener("click", function () {
+    setTimeout(function () {
+        this.style.backgroundColor = "Green";
+    }.bind(this), 3000);
+});
+
+
+const myButton4 = document.getElementById("myButton4");
+myButton4.addEventListener("click", function () {
+    setTimeout(() => {
+        this.style.backgroundColor = "Green";
+    }, 3000);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function doSomething(callback) {
+
+}
+
+
+doSomething(function () { });
+doSomething(() => { });
+
+
+const x1 = function () { };
+const x2 = () => { };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
