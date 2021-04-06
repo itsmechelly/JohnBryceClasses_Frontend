6dbdc29add449446dd4ef28e91563e45bbe23e0a
Chelly izraelov <hlly.lov@gmail.com>
2021-04-06 14:42:44 +0300

    ...

---
+++ 04_React_Ac45/northwind/src/Models/ProductModel.ts
@@ -0,0 +1,9 @@
+class ProductModel {
+    public id: number;
+    public name: string;
+    public price: number;
+    public stock: number;
+    public imageName: string;
+}
+
+export default ProductModel;
