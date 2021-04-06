8b25bb6d449e19eb250291c1222f95f8e7a95b25
Chelly izraelov <hlly.lov@gmail.com>
2021-04-06 14:41:07 +0300

    ...

--- 04_React_Ac45/northwind/src/Components/LayoutArea/Layout/Layout.tsx
+++ 04_React_Ac45/northwind/src/Components/LayoutArea/Layout/Layout.tsx
@@ -1,32 +1,36 @@
+import { BrowserRouter } from "react-router-dom";
 import ContactUs from "../../ContactUsArea/ContactUs/ContactUs";
 import Home from "../../HomeArea/Home/Home";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import Menu from "../Menu/Menu";
+import Routing from "../Routing/Routing";
 import "./Layout.css";
 
 function Layout(): JSX.Element { //JSX.Element = the component UI
     return (
+        /* BrowseRouter - Wrapper over all routes components */
+        <BrowserRouter>
         <div className="Layout">
 
             <header>
-                <Header></Header>
+                <Header />
             </header>
 
             <aside>
-                <Menu></Menu>
+                <Menu />
             </aside>
 
             <main>
-                {/* <Home></Home> */}
-                <ContactUs></ContactUs>
+                <Routing />
             </main>
 
             <footer>
-                <Footer></Footer>
+                <Footer />
             </footer>
 
         </div>
+        </BrowserRouter>
     );
 }
 
