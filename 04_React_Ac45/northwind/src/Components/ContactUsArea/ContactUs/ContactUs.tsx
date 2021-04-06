1c2ea1b18b6f4753f1c42b3f212c85ffc55abebf
Chelly izraelov <hlly.lov@gmail.com>
2021-04-06 18:30:41 +0300

    ...

--- 04_React_Ac45/northwind/src/Components/ContactUsArea/ContactUs/ContactUs.tsx
+++ 04_React_Ac45/northwind/src/Components/ContactUsArea/ContactUs/ContactUs.tsx
@@ -8,21 +8,21 @@ function ContactUs(): JSX.Element {
 
             <Typography variant="h3" className="Headline"> <MailOutline />Contact Us</Typography>
 
-            <TextField label="Name" variant="outlined" className="TextBox"/>
+            <TextField label="Name" variant="outlined" className="TextBox" />
             <br />
 
-            <TextField label="Email" variant="outlined" type="email" className="TextBox"/>
+            <TextField label="Email" variant="outlined" type="email" className="TextBox" />
             <br />
 
-            <TextField label="Message" variant="outlined" className="TextBox"/>
+            <TextField label="Message" variant="outlined" className="TextBox" />
             <br />
 
             <FormControlLabel label="Send me promotional emails: " control={<Checkbox />} />
             <br />
 
             <ButtonGroup variant="contained" fullWidth>
-            <Button color="primary" startIcon={<Send />}>Send</Button>
-            <Button color="secondary" startIcon={<Cancel />}>Cancel</Button>
+                <Button color="primary" startIcon={<Send />}>Send</Button>
+                <Button color="secondary" startIcon={<Cancel />}>Cancel</Button>
             </ButtonGroup>
 
         </div>
