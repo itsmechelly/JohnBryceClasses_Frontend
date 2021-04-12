beb6456d3b99ecd48e67bc2027b752f9fb56143f
Chelly izraelov <hlly.lov@gmail.com>
2021-04-12 10:19:51 +0300

    ...

--- 04_React_Ac45/northwind/src/Components/ProductsArea/AddProduct/AddProduct.tsx
+++ 04_React_Ac45/northwind/src/Components/ProductsArea/AddProduct/AddProduct.tsx
@@ -11,12 +11,12 @@ function AddProduct(): JSX.Element {
     // const obj = useForm<ProductModel>();
     // const register = obj.register;
     // const handleSubmit = obj.handleSubmit;
-    const { register, handleSubmit } = useForm<ProductModel>();
+    const { register, handleSubmit, errors } = useForm<ProductModel>();
     const history = useHistory();
 
     async function send(product: ProductModel) {
         try {
-            
+
             //Convert ProductModel to FormData in order to send text + image to the backend:
             const myFormData = new FormData();
             myFormData.append("name", product.name);
@@ -27,7 +27,7 @@ function AddProduct(): JSX.Element {
             const response = await axios.post<ProductModel>(globals.urls.products, myFormData);
             const addedProduct = response.data;
             alert("Product has been added! id: " + addedProduct.id);
-        
+
             history.push("/products");
         } catch (error) {
             alert("Error...");
@@ -36,32 +36,41 @@ function AddProduct(): JSX.Element {
 
     return (
         <div className="AddProduct">
-			
+
             <h2>Add Product</h2>
 
             <form onSubmit={handleSubmit(send)}>
 
                 <label>Name: </label>
                 <br />
-                <input type="text" name="name" ref={register} />
+                <input type="text" name="name" ref={register({ required: true, minLength: 3, pattern: /^[A-Z].+$/ })} />
+                {errors.name?.type === "required" && <span>Missing name.</span>}
+                {errors.name?.type === "minLength" && <span>Name too short.</span>}
+                {errors.name?.type === "pattern" && <span>Name must start with a capital letter.</span>}
                 <br />
                 <br />
 
                 <label>Price: </label>
                 <br />
-                <input type="number" step="0.01" name="price" ref={register} />
+                <input type="number" step="0.01" name="price" ref={register({
+                    required: { value: true, message: "Missing price." },
+                    min: { value: 0, message: "Price can't be negative." }
+                })} />
+                {errors.price && <span>Missing price.</span>}
                 <br />
                 <br />
 
                 <label>Stock: </label>
                 <br />
-                <input type="number" name="stock" ref={register} />
+                <input type="number" name="stock" ref={register({ required: true, minLength: 3 })} />
+                {errors.stock && <span>Missing stock.</span>}
                 <br />
                 <br />
 
                 <label>Image: </label>
                 <br />
-                <input type="file" name="image" ref={register} accept="image/*" />
+                <input type="file" name="image" ref={register({ required: true })} accept="image/*" />
+                {errors.image && <span>Missing image.</span>}
                 <br />
                 <br />
 
