cd42fc5823f4a9ef7ffc80fb8f02585f31312616
Chelly izraelov <hlly.lov@gmail.com>
2021-04-12 14:40:22 +0300

    ...

--- 04_React_Ac45/northwind/src/Components/LayoutArea/Footer/Footer.tsx
+++ 04_React_Ac45/northwind/src/Components/LayoutArea/Footer/Footer.tsx
@@ -1,8 +1,10 @@
+import TotalProducts from "../../ProductsArea/TotalProducts/TotalProducts";
 import "./Footer.css";
 
 function Footer(): JSX.Element {
     return (
         <div className="Footer">
+            <TotalProducts />
 			<p>All Rights Reserved &copy;</p>
         </div>
     );
--- 04_React_Ac45/northwind/src/Components/ProductsArea/TotalProducts/TotalProducts.tsx
+++ 04_React_Ac45/northwind/src/Components/ProductsArea/TotalProducts/TotalProducts.tsx
@@ -1,23 +1,28 @@
 import { Component } from "react";
+import store from "../../../Redux/Store";
 import "./TotalProducts.css";
 
 interface TotalProductsState {
-	
+	count: number;
 }
 
 class TotalProducts extends Component<{}, TotalProductsState> {
 
     public constructor(props: {}) {
         super(props);
-        this.state = {
-			
-        };
+        this.state = {count: 0 };
+    }
+
+    public componentDidMount(): void {
+        store.subscribe(()=>{
+            this.setState({count: store.getState().ProductsState.products.length});
+        });
     }
 
     public render(): JSX.Element {
         return (
             <div className="TotalProducts">
-				
+				<span>Total Products: {this.state.count}</span>
             </div>
         );
     }
