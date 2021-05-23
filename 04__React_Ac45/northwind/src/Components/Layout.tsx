6f93a334695c44bba0517fd74cceac2499d3eced
Chelly izraelov <hlly.lov@gmail.com>
2021-05-23 11:21:55 +0300

    ...

--- 04__React_Ac45/northwind/src/Components/Layout.tsx
+++ 04__React_Ac45/northwind/src/Components/Layout.tsx
@@ -5,9 +5,12 @@ function Layout(): JSX.Element { //JSX.Element = The component UI
     return (
     
         <div className="Layout">
-            <p>Text...</p>
+            <header>1</header>
+            <aside>2</aside>
+            <main>3</main>
+            <footer>4</footer>
         </div>
-        );
+    );
 }
 
 export default Layout;
