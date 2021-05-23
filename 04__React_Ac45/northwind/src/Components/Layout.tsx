38760871edba00c40fe002dd497f3fe4abb87d02
Chelly izraelov <hlly.lov@gmail.com>
2021-05-23 10:55:10 +0300

    ...

---
+++ 04__React_Ac45/northwind/src/Components/Layout.tsx
@@ -0,0 +1,10 @@
+import "./Layout.css";
+
+function Layout(): JSX.Element { //JSX.Element = The component UI
+
+    return
+    <div>
+        <p>Cool Text1</p>
+        <p>Cool Text2</p>
+    </div>;
+}
