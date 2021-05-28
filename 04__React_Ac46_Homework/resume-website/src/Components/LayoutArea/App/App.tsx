454733618a85bc46121b1c8a508e0e2f3463a6eb
Chelly izraelov <hlly.lov@gmail.com>
2021-05-28 14:04:46 +0300

    ...

--- 04__React_Ac46_Homework/resume-website/src/Components/LayoutArea/App/App.tsx
+++ 04__React_Ac46_Homework/resume-website/src/Components/LayoutArea/App/App.tsx
@@ -5,7 +5,7 @@ import "./App.css";
 
 function App(): JSX.Element {
     return (
-        <div className="Apkp">
+        <div className="App">
             <header>
                 <Header />
             </header>
