4e11eed799d9ffb36d489c151fc92ff202865eae
Chelly izraelov <hlly.lov@gmail.com>
2021-05-28 14:19:55 +0300

    ...

---
+++ 04__React_Ac46/02_Callback&Arrows/main.js
@@ -0,0 +1,25 @@
+function test() {
+    doSomething1(function () {
+        alert("Hi");
+    });
+    doSomething2(function (a) {
+        alert(a);
+    });
+    doSomething2(function (a, b) {
+        alert(a + b);
+    });
+    doSomething3(function (a, b) {
+        return a + b;
+    })
+}
+
+function doSomething1(callback) {
+    callback(); //Call it Back (callback)
+}
+function doSomething2(callback) {
+    callback(10, 20);
+}
+function doSomething3(callback) {
+    const sum = callback(10, 20);
+    console.log(sum);
+}
