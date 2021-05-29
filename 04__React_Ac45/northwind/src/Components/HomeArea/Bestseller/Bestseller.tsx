f44c820534ead3d8599592cc22994a12f010e113
Chelly izraelov <hlly.lov@gmail.com>
2021-05-29 13:05:35 +0300

    ...

--- 04__React_Ac45/northwind/src/Components/HomeArea/Bestseller/Bestseller.tsx
+++ 04__React_Ac45/northwind/src/Components/HomeArea/Bestseller/Bestseller.tsx
@@ -1,23 +1,31 @@
 import { Component } from "react";
 import "./Bestseller.css";
 
-//goes with....
-// interface BestSellerProps{}
-
 interface BestsellerState {
     item: string;
     price: number;
 }
 
-//goes with....
-//class Bestseller extends Component<{}, BestsellerState> {}
+class Bestseller extends Component<{}, BestsellerState> {
+    
+    private isClicked: boolean = false;
 
-class Bestseller extends Component {
+    public constructor(props: {}) {
+        super(props);
+        this.state = { item: "", price: 0 }; //Initializing the state - can be done only here!
+    }
+
+    private showItem = () => {
+        //setState here is a-synchronic!
+        this.isClicked = true;
+        this.setState({ item: "Irish Coffee", price: 22 });
+    }
 
     public render(): JSX.Element {
         return (
-            <div className="Bestseller">
-				
+            <div className="Bestseller Box">
+                <button onClick={this.showItem}>Bestseller Product</button>
+                {this.isClicked && <span>{this.state.item}, price: {this.state.price}</span>}
             </div>
         );
     }
