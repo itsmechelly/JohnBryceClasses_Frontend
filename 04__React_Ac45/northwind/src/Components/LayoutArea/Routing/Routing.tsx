7ec54b208e783f67f14faee61d8bde00d92492cf
Chelly izraelov <hlly.lov@gmail.com>
2021-06-13 11:00:53 +0300

    ...

--- 04__React_Ac45/northwind/src/Components/LayoutArea/Routing/Routing.tsx
+++ 04__React_Ac45/northwind/src/Components/LayoutArea/Routing/Routing.tsx
@@ -6,7 +6,7 @@ import About from "../../AboutArea/About/About";
 import ContactUs from "../../ContactUsArea/ContactUs/ContactUs";
 import Home from "../../HomeArea/Home/Home";
 // import AddProduct from "../../ProductsArea/AddProduct/AddProduct";
-// import ProductDetails from "../../ProductsArea/ProductDetails/ProductDetails";
+import ProductDetails from "../../ProductsArea/ProductDetails/ProductDetails";
 import ProductList from "../../ProductsArea/ProductList/ProductList";
 import Page404 from "../../SharedArea/Page404/Page404";
 
@@ -16,8 +16,8 @@ function Routing(): JSX.Element {
             <Switch>
                 <Route path="/home" component={Home} exact />
                 <Route path="/products" component={ProductList} exact />
-                {/* <Route path="/products/details/:id" component={ProductDetails} exact />
-                <Route path="/products/new/" component={AddProduct} exact /> */}
+                <Route path="/products/details/:id" component={ProductDetails} exact />
+                {/* <Route path="/products/new/" component={AddProduct} exact /> */}
                 <Route path="/about" component={About} exact />
 
 
