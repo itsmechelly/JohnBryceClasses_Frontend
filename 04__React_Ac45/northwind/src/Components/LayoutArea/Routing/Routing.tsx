b644811ccc78ad15e6452d2dee8413b5b250b64d
Chelly izraelov <hlly.lov@gmail.com>
2021-06-16 11:22:22 +0300

    ...

--- 04__React_Ac45/northwind/src/Components/LayoutArea/Routing/Routing.tsx
+++ 04__React_Ac45/northwind/src/Components/LayoutArea/Routing/Routing.tsx
@@ -1,8 +1,8 @@
-import { Redirect, Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router";
 import About from "../../AboutArea/About/About";
-// import Login from "../../AuthArea/Login/Login";
-// import Logout from "../../AuthArea/Logout/Logout";
-// import Register from "../../AuthArea/Register/Register";
+import Login from "../../AuthArea/Login/Login";
+import Logout from "../../AuthArea/Logout/Logout";
+import Register from "../../AuthArea/Register/Register";
 import ContactUs from "../../ContactUsArea/ContactUs/ContactUs";
 import Home from "../../HomeArea/Home/Home";
 import AddProduct from "../../ProductsArea/AddProduct/AddProduct";
@@ -14,6 +14,7 @@ function Routing(): JSX.Element {
     return (
         <div className="Routing">
             <Switch>
+                <Redirect from="/" to="/home" exact />
                 <Route path="/home" component={Home} exact />
                 <Route path="/products" component={ProductList} exact />
                 <Route path="/products/details/:id" component={ProductDetails} exact />
@@ -21,16 +22,13 @@ function Routing(): JSX.Element {
                 <Route path="/about" component={About} exact />
 
 
-                {/* <Route path="/register" component={Register} exact />
+                <Route path="/register" component={Register} exact />
                 <Route path="/login" component={Login} exact />
-            <Route path="/logout" component={Logout} exact /> */}
+                <Route path="/logout" component={Logout} exact />
 
 
                 <Route path="/contact-us" component={ContactUs} exact />
-
-                <Redirect from="/" to="/home" exact />
                 <Route component={Page404} />
-
             </Switch>
         </div>
     );
